f970f390f3e19dea47fd6304dfb8ea05bfe0c2a3
Neil Gebhard <neilgebhard@gmail.com>
2021-07-25 12:11:33 +1000

    :sparkles: Add useState() example

---
+++ src/UseStateExample.js
@@ -0,0 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+const UseStateExample = () => {
+    const codeSnippet = `
+        function App() {
+            // the initial state is passed in as an argument
+            // useState() returns the current state and a function that updates it
+            const [count, setCount] = useState(0);
+
+            const handleClick = () => {
+                setCounter(count + 1);
+            };
+
+            return (
+                <div>
+                    <div>{count}</div>
+                    <button onClick={handleClick}>Increment</button>
+                </div>
+            );
+        };
+    `;
+
+    return (
+        <div>
+        <a href="https://reactjs.org/docs/hooks-state.html">useState()</a>
+        <SyntaxHighlighter language="javascript" style={docco}>
+            {codeSnippet}
+        </SyntaxHighlighter>
+        </div>
+    );
+};
+
+export default UseStateExample;
